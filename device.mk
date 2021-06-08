eb06e4bee99153bf3374420d288aa52aa12b322f
Xirudly <734428735@qq.com>
2021-06-08 20:07:54 +0800

    cepheus: exTHmUI Bringup Signed-off-by: Xirudly <734428735@qq.com>

--- device.mk
+++ device.mk
@@ -339,7 +339,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pe
+    $(LOCAL_PATH)/overlay-exthm
 
 # Power
 PRODUCT_PACKAGES += \
